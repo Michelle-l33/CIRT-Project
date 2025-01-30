5ebe3d4c190ceb4168b666d700329b9abbdcd5fb
Michelle-l33 <michelle.lee@spartans.ut.edu>
2025-01-30 11:37:00 -0500

    Added Page Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Components/Home';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
+       {/* <NavBar/>  This makes it so that the navbar is constant on every page */}
+        <Routes> {/* Routes are part of the URL; each route path is what takes you to the right component */}
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
