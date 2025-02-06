73c380f1d564a87c2a8855ea46f672045782a59c
Michelle-l33 <michelle.lee@spartans.ut.edu>
2025-02-06 11:48:59 -0500

    login route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
+import Login from './Components/Login/Login';
 //import NavBar from './Components/NavBar'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
        {/* <NavBar/>  This makes it so that the navbar is constant on every page */}
         <Routes> {/* Routes are part of the URL; each route path is what takes you to the right component */}
           <Route path="/" element={<Home />} />
+          <Route path = "/login" element={<Login />} />
         </Routes>
       </Router>
     </>
