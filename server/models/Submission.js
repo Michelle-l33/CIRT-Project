bb71ecf3c446b3a614ac43d753111993507ffbd4
Michelle-l33 <michelle.lee@spartans.ut.edu>
2025-02-13 12:41:21 -0500

    Update Submission.js

--- server/models/Submission.js
+++ server/models/Submission.js
@@ -9,7 +9,13 @@ const submissionSchema = new mongoose.Schema({
   document:{type:String, required:true},
   isPoster:{type: Boolean,default:false },
   isArticle:{type:Boolean, default:false},
-  abstract: {type:String}
+  abstract: {type:String},
+  stage: {type:Integer, default: 1} 
+  // stage 1: new submission ("Just Submitted!")
+  //stage 2: sent to reviewer ("Being Reviewed!")
+  //stage 3: returned from reviewer (only for editor to see??)
+  //stage 4: edits suggested to author("Final Edits")
+  // stage 5: Submitted("Published!")
   
 });
 
