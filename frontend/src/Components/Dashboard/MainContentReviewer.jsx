931e11a2173172b8d89730db29535483722bc74c
Horizon489732 <tranminhhien27102003@gmail.com>
2025-02-27 11:14:59 -0500

    submission URL change

--- frontend/src/Components/Dashboard/MainContentReviewer.jsx
+++ frontend/src/Components/Dashboard/MainContentReviewer.jsx
@@ -20,9 +20,35 @@ const MainContentReviewer = () => {
                     <h1>Dashboard Reviewer - This is so painful</h1>                
                 </div>
 
-                {/* <div className={styles.content}>
-                    <Outlet />
-                </div> */}
+                <div className={styles.content}>
+                    <ul className={styles.listHeader}>
+                        <li>Title</li>
+                        <li>Date Assigned</li>
+                        <li>Editor</li>
+                        <li>Due Date?</li>
+                    </ul>
+
+                    <ul className = {styles.submissionContaniner}>
+                        <li className = {styles.submission}>
+                            <h4>fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
+                            <span>10/23</span>
+                            <p>Editor's name</p>
+                            <time>10/27</time>
+                        </li>
+                        <li className = {styles.submission}>
+                            <h4>fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
+                            <span>10/23</span>
+                            <p>Editor's name</p>
+                            <time>10/27</time>
+                        </li>
+                        <li className = {styles.submission}>
+                            <h4>fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
+                            <span>10/23</span>
+                            <p>Editor's name</p>
+                            <time>10/27</time>
+                        </li>
+                    </ul>
+                </div>
                 
                 
             </main>
