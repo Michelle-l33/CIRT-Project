267db1f3a02ed0b84f06900dc8ec0a87e5608af9
Horizon489732 <tranminhhien27102003@gmail.com>
2025-03-15 14:16:43 -0400

    Working on the Reviewer Dashboard

---
+++ frontend/src/Components/Dashboard/ReviewerAssignment/All.jsx
@@ -0,0 +1,37 @@
+import styles from "./ReviewerAssignment.module.css"
+
+const All = () => {
+    return (
+        <div className={styles.currentAssignemt}>
+            <ul className={styles.listHeader}>
+                <li>Title</li>
+                <li>Date Assigned</li>
+                <li>Editor</li>
+                <li>Due Date?</li>
+            </ul>
+
+            <ul className = {styles.submissionContainer}>
+                <li className = {styles.submission}>
+                    <h4>All fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
+                    <span>10/23</span>
+                    <p>Editor's name</p>
+                    <time>10/27</time>
+                </li>
+                <li className = {styles.submission}>
+                    <h4>All fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
+                    <span>10/23</span>
+                    <p>Editor's name</p>
+                    <time>10/27</time>
+                </li>
+                <li className = {styles.submission}>
+                    <h4>All fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
+                    <span>10/23</span>
+                    <p>Editor's name</p>
+                    <time>10/27</time>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default All;
