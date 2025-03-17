d62632d62be7db3bba5b2a49c55c2e24b3c8094e
Horizon489732 <tranminhhien27102003@gmail.com>
2025-03-17 09:28:11 -0400

    Added Layouts to the DashboardReviewer
    Still need to do some styling and more functionalities

--- frontend/src/Components/Dashboard/ReviewerAssignment/Current.jsx
+++ frontend/src/Components/Dashboard/ReviewerAssignment/Current.jsx
@@ -1,6 +1,76 @@
+import { useState } from "react";
 import styles from "./ReviewerAssignment.module.css"
+import SubDetail from "./SubmissionDetails";
+
+const listOfSubmission = [
+    {
+        title: "1 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-23"),
+        editorFName: "John",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-27"),
+    },
+    {
+        title: "2 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-24"),
+        editorFName: "Jack",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-27"),
+    },
+    {
+        title: "3 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-23"),
+        editorFName: "Jay",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-25"),
+    },
+    {
+        title: "4 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-26"),
+        editorFName: "Jacky",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-27"),
+    },
+    {
+        title: "5 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-21"),
+        editorFName: "Jishy",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-27"),
+    },
+    {
+        title: "6 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-26"),
+        editorFName: "Jojo",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-24"),
+    },
+    {
+        title: "6 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-26"),
+        editorFName: "Jojo",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-24"),
+    },
+    {
+        title: "6 fsdfh sdjgfdfh dfhgs asdgasf sdfhg",
+        dateAssigned: new Date("2025-10-26"),
+        editorFName: "Jojo",
+        editorLName: "Smith",
+        dueDate: new Date("2025-10-24"),
+    },
+
+]
 
 const Current = () => {
+    function getDate(date) {
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return `${month}/${day}`;
+    }
+
+    const [currSubmission, setCurrSubmission] = useState(null)
+
     return (
         <div className={styles.currentAssignemt}>
             <ul className={styles.listHeader}>
@@ -11,25 +81,16 @@ const Current = () => {
             </ul>
 
             <ul className = {styles.submissionContainer}>
-                <li className = {styles.submission}>
-                    <h4>fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
-                    <span>10/23</span>
-                    <p>Editor's name</p>
-                    <time>10/27</time>
-                </li>
-                <li className = {styles.submission}>
-                    <h4>fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
-                    <span>10/23</span>
-                    <p>Editor's name</p>
-                    <time>10/27</time>
-                </li>
-                <li className = {styles.submission}>
-                    <h4>fsdfh sdjgfdfh dfhgs asdgasf sdfhg</h4>
-                    <span>10/23</span>
-                    <p>Editor's name</p>
-                    <time>10/27</time>
-                </li>
+                {listOfSubmission.map((sub, idx) => (
+                    <li className = {styles.submission} key = {idx} onClick = {(() => setCurrSubmission(sub))}>
+                        <h4>{sub.title}</h4>
+                        <time>{getDate(sub.dateAssigned)}</time>
+                        <p>{sub.editorFName +" "+ sub.editorLName}</p>
+                        <time>{getDate(sub.dueDate)}</time>
+                    </li>
+                ))}
             </ul>
+            { currSubmission != null && <SubDetail submission = {currSubmission} setIsOpen={setCurrSubmission}/> }
         </div>
     )
 }
