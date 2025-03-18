9e3bb1b706b472dac2fc17dff37bc6a4b42cc5a9
Horizon489732 <tranminhhien27102003@gmail.com>
2025-03-18 16:15:51 -0400

    Styling the HomePage

--- frontend/src/Components/HomePage/SearchBar/SearchBar.jsx
+++ frontend/src/Components/HomePage/SearchBar/SearchBar.jsx
@@ -7,13 +7,14 @@ const SearchBarHome =() => {
     return( // html goes in the return()
 
         <div className = {styles.bigContainer}>
-            <h2>This is where you search!</h2>
+            <h2 className = {styles.searchHeading}>This is where you search!</h2>
 
             <div className = {styles.searchContainer}>
                 <SearchBar />
             </div>
 
+            <img className = {styles.searchPic} src='https://art.pixilart.com/4ba12e999f3819b.png' />
         </div>
     );
 };
-export default SearchBarHome; //make sure to export the function
+export default SearchBarHome;
