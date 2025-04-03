0df6c473936d098f008e6ba820f5bbcc45506484
Michelle-l33 <michelle.lee@spartans.ut.edu>
2025-04-03 12:28:41 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 8082;
 const corsOptions = {
   origin: ['http://localhost:3000', 'https://cirt-project.vercel.app'],  // Allowed origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'User-Agent', 'Accept', 'Referer'], 
   credentials: true,  // Allow cookies to be sent with requests
 };
 
