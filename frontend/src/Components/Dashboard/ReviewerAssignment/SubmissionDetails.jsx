3b9104c9aa0388efec8fb8a71cbe44ad61a4e2e1
Michelle-l33 <michelle.lee@spartans.ut.edu>
2025-04-03 20:49:00 -0400

    Update SubmissionDetails.jsx

--- frontend/src/Components/Dashboard/ReviewerAssignment/SubmissionDetails.jsx
+++ frontend/src/Components/Dashboard/ReviewerAssignment/SubmissionDetails.jsx
@@ -16,7 +16,7 @@ const SubDetail = ({submission, setIsOpen}) => {
     const originalSubmissionID = queryParams.get('submission');
     const [ searchParams, setSearchParams ] = useSearchParams();
    
-    //fetches comments
+    
     const fetchComments = async () => {
         try {
             console.log("Fetching for: ", user._id);
@@ -39,7 +39,7 @@ const SubDetail = ({submission, setIsOpen}) => {
     useEffect(()=>{
         
         fetchComments();
-    },[originalSubmissionID]);
+    },[submission]);
 
     const Comment = ({content, sender}) => {
         return(
