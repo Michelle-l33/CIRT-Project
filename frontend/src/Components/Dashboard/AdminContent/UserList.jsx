40213e384cd2050d29743fc022b2e625560669f1
Horizon489732 <tranminhhien27102003@gmail.com>
2025-04-26 13:10:46 -0400

    small changes

--- frontend/src/Components/Dashboard/AdminContent/UserList.jsx
+++ frontend/src/Components/Dashboard/AdminContent/UserList.jsx
@@ -56,62 +56,75 @@ const AddUserBtn = () => {
             <div className = {`${styles.userFormContainer} ${isOpen ? styles.show : ""}`}>
                 <h4>Add User</h4>
                 
-                <form className = {styles.userForm}>
-                    <div className={styles.inputContainer}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            placeholder="Username"
-                            maxLength={50}
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="you@example.com"
-                            maxLength={50}
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="text" 
-                            id="password"
-                            name="password"
-                            placeholder="Password"
-                            maxLength={25}
-                            required
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="accountType">Account Type:</label>
-                            <select
-                                name="accountType"
-                                id="accountType"
-                                required
-                            >
-                            <option value="author">Author</option>
-                            <option value="editor">Editor</option>
-                            <option value="reviewer">Reviewer</option>
-                            <option value="admin">Admin</option>
-                        </select>
-                     </div>
-
-                    <button type="submit">Create User</button>
-                </form>
+                {/* the code for UserForm is defined down below */}
+                {isOpen && <UserForm onClose={() => setIsOpen(false)} />}
 
                 <button className={styles.closeBtn} onClick = {() => setIsOpen(null)}>&#215;</button>
             </div>
         </div>
     )
 }
+
+const UserForm = ({ onClose }) => {
+    return (
+        <>
+            <form className={styles.userForm}>
+                <div className={styles.inputContainer}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Username"
+                        maxLength={50}
+                        required
+                    />
+                </div>
+
+                <div className={styles.inputContainer}>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="you@example.com"
+                        maxLength={50}
+                        required
+                    />
+                </div>
+
+                <div className={styles.inputContainer}>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        maxLength={25}
+                        required
+                    />
+                </div>
+
+                <div className={styles.inputContainer}>
+                    <label htmlFor="accountType">Account Type:</label>
+                    <select
+                        name="accountType"
+                        id="accountType"
+                        required
+                    >
+                        <option value="author">Author</option>
+                        <option value="editor">Editor</option>
+                        <option value="reviewer">Reviewer</option>
+                        <option value="admin">Admin</option>
+                    </select>
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+
+            <button className={styles.closeBtn} onClick={onClose}>&#215;</button>
+        </>
+    );
+};
 
 export default UserList;
